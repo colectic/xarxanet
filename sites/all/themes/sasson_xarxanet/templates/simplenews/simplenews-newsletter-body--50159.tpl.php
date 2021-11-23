709a10d0b90dbc8f69d6deaed4bdb81dccd303ef
Berta Nicolau <berta.nicolau@tothomweb.com>
2021-11-23 13:38:43 +0100

    Solució d'errors de butlletins en Outlook

--- sites/all/themes/sasson_xarxanet/templates/simplenews/simplenews-newsletter-body--50159.tpl.php
+++ sites/all/themes/sasson_xarxanet/templates/simplenews/simplenews-newsletter-body--50159.tpl.php
@@ -152,6 +152,11 @@ ksort($financ_nodes);
 <!-- @END CSS Styles from TOTHOMweb -->
 <!-- @END CSS Styles from TOTHOMweb -->
 <center id="newsletter" class="wrapper" style="width:100%;table-layout:fixed;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;">
+<!--[if (gte mso 9)|(IE)]>
+		<table width="600" align="center" style="border-spacing:0;font-family:sans-serif;color:#333333;">
+		<tr>
+		<td style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;">
+	<![endif]-->
 	<div class="webkit" style="max-width:602px;margin-top:0;margin-bottom:0;margin-right:auto;margin-left:auto;">
 		<table>
 			<tr>
@@ -162,19 +167,19 @@ ksort($financ_nodes);
 				</td>
 			</tr>
 		</table>	
-		<table class="butlleti" style="font-family: Fira Sans,Helvetica,Arial,sans-serif; font-size: 13px; border:1px solid #d3d3d2; border-bottom: 0px; width:100%;">
+		<table class="butlleti" style="background-color:#2f3031;font-family: Fira Sans,Helvetica,Arial,sans-serif; font-size: 13px; border:1px solid #d3d3d2; border-bottom: 0px; width:100%;">
 			<!-- CAPÇALERA -->
-			<tr style="background-color:#2f3031;">
+			<tr>
 				<td style="padding: 0 0 0 5px;">
 					<a href="http://www.xarxanet.org" style="text-decoration:none">
-						<img src="<?php print $pathroot; ?>/sites/all/themes/xn17/logo.png" alt="logotip xarxanet" style="margin-left:5px; margin-top:20px" />
+						<img src="<?php print $pathroot; ?>/sites/all/themes/xn17/logo.png" alt="Xarxanet" style="margin-left:5px; margin-top:20px" />
 					</a>
 				</td>
 				<td>
-					<p style="font-size:38px; color:#FFFFFF; text-align:right; font-weight:bold; margin:10px 5px">Finançament</p>
+					<p style="font-size:28px; color:#FFFFFF; text-align:right; font-weight:bold; margin:10px 5px">Finançament</p>
 				</td>
 			</tr>
-			<tr style="background-color:#2f3031; color:#878787; font-weight:bold;">
+			<tr style="color:#878787; font-weight:bold;">
 				<td style="padding: 5px 10px; border-top:3px solid #231f20; border-bottom: 15px solid white;">
 					<?php
 					$created = $node->created;
@@ -182,9 +187,11 @@ ksort($financ_nodes);
 					?>
 				</td>
 				<td style="text-align: right; padding: 5px 10px; border-top:3px solid #231f20; border-bottom: 15px solid white;">
-					<a href="http://www.xarxanet.org/hemeroteca_actualitat" style=" color:#878787">Butlletins anteriors</a>
+					<a href="http://www.xarxanet.org/hemeroteca_financament" style=" color:#878787">Butlletins anteriors</a>
 				</td>
 			</tr>
+		</table>
+		<table class="butlleti" style="font-family: Fira Sans,Helvetica,Arial,sans-serif; font-size: 13px; border:1px solid #d3d3d2; border-bottom: 0px; width:100%;">
 			<?php if (!empty($highlightnew1Node)) : ?>
 			<tr>
 				<td colspan="2" style="padding: 30px 15px 30px 15px; vertical-align: top; ">
@@ -195,13 +202,13 @@ ksort($financ_nodes);
 									<table colspan="2">
 										<tr>
 											<td style="padding:20px 15px 0 20px;">
-												<h3 style="font-size:1.25em; font-weight:600; margin-top: 0; margin-bottom:17px; line-height:1.2em;"><?php echo $highlightnew1Title; ?></h3>
-												<p style="font-size: .874em; line-height: 1.22em; margin:15px 0 0 0;"><?php echo $highlightnew1Text; ?></p>
+												<h3 style="font-family:Fira Sans,Helvetica,Arial,sans-serif!important;font-size:1.25em; font-weight:600; margin-top: 0; margin-bottom:17px; line-height:1.2em;"><?php echo $highlightnew1Title; ?></h3>
+												<p style="font-family:Fira Sans,Helvetica,Arial,sans-serif!important;font-size: .874em; line-height: 1.22em; margin:15px 0 0 0;"><?php echo $highlightnew1Text; ?></p>
 											</td>
 										</tr>
 										<tr>
 											<td style="padding:17px 0 20px 20px;">
-												<a href="<?php echo $highlightnew1Link; ?>" style="background-color:#BE1622; color:#ffffff; font-size:14px; padding:14px 16px;float: left; border-radius: 5px; text-decoration:none;">
+												<a href="<?php echo $highlightnew1Link; ?>" style="font-family:Fira Sans,Helvetica,Arial,sans-serif!important;background-color:#BE1622; color:#ffffff; font-size:14px; border:15px solid #BE1622;float: left; border-radius: 5px; text-decoration:none;">
 													Llegiu-ne més
 												</a>
 											</td>
@@ -210,7 +217,14 @@ ksort($financ_nodes);
 								</td>
 								<td style="padding:20px 20px 20px 10px; border-radius: 10px; vertical-align:top;">
 									<a href="<?php print $highlightnew1Link; ?>">
+									<!--[if (gte mso 9)|(IE)]>
+										<img src="<?php print $highlightnew1Image; ?>" width="250" alt="<?php print $highlightnew1ImageAlt; ?>" style="border-width:0;font-family:Fira Sans, Helvetica, Arial !important;width:100%;max-width:600px;height:auto; border-radius:15px;" />
+										<div style="display:none">
+										<![endif]-->																					
 										<img src="<?php print $highlightnew1Image; ?>" width="600" alt="<?php print $highlightnew1ImageAlt; ?>" style="border-width:0;font-family:Fira Sans, Helvetica, Arial !important;width:100%;max-width:600px;height:auto; border-radius:15px;" />
+									<!--[if mso]>
+                                        </div>
+                                    <![endif]-->
 									</a>
 								</td>
 							</tr>
@@ -227,20 +241,27 @@ ksort($financ_nodes);
 							<tr>
 							<td style="padding:20px 10px 20px 20px; border-radius: 10px; vertical-align:top;">
 									<a href="<?php print $highlightnew2Link; ?>">
+									<!--[if (gte mso 9)|(IE)]>
+										<img src="<?php print $highlightnew2Image; ?>" width="250" alt="<?php print $highlightnew2ImageAlt; ?>" style="border-width:0;font-family:Fira Sans, Helvetica, Arial !important;width:100%;max-width:600px;height:auto; border-radius:15px;" />
+										<div style="display:none">
+										<![endif]-->												
 										<img src="<?php print $highlightnew2Image; ?>" width="600" alt="<?php print $highlightnew2ImageAlt; ?>" style="border-width:0;font-family:Fira Sans, Helvetica, Arial !important;width:100%;max-width:600px;height:auto; border-radius:15px;" />
+									<!--[if mso]>
+                                        </div>
+                                    <![endif]-->
 									</a>
-								</td>
+								</td	>
 								<td style="width: 55%;">
 									<table colspan="2">
 										<tr>
 											<td style="padding:20px 15px 0 20px;">
-												<h3 style="font-size:1.25em; font-weight:600; margin-top: 0; margin-bottom:17px; line-height:1.2em;"><?php echo $highlightnew2Title; ?></h3>
-												<p style="font-size: .874em; line-height: 1.22em; margin:15px 0 0 0;"><?php echo $highlightnew2Text; ?></p>
+												<h3 style="font-family:Fira Sans,Helvetica,Arial,sans-serif!important;font-size:1.25em; font-weight:600; margin-top: 0; margin-bottom:17px; line-height:1.2em;"><?php echo $highlightnew2Title; ?></h3>
+												<p style="font-family:Fira Sans,Helvetica,Arial,sans-serif!important;font-size: .874em; line-height: 1.22em; margin:15px 0 0 0;"><?php echo $highlightnew2Text; ?></p>
 											</td>
 										</tr>
 										<tr>
 											<td style="padding:17px 0 20px 20px;">
-												<a href="<?php echo $highlightnew2Link; ?>" style="background-color:#BE1622; color:#ffffff; font-size:14px; padding:14px 16px;float: left; border-radius: 5px; text-decoration:none;">
+												<a href="<?php echo $highlightnew2Link; ?>" style="font-family:Fira Sans,Helvetica,Arial,sans-serif!important;background-color:#BE1622; color:#ffffff; font-size:14px; border:15px solid #BE1622;float: left; border-radius: 5px; text-decoration:none;">
 													Llegiu-ne més
 												</a>
 											</td>
@@ -267,24 +288,24 @@ ksort($financ_nodes);
 						<?php foreach ($financ_nodes as $financ_node) : ?>
 							<tr>
 								<td>
-									<table style="font-family: Fira Sans,Helvetica,Arial,sans-serif; font-size: 16px; width:100%;  background-color:#FFFFFF; border-radius:15px; margin-bottom:25px; color:#252627;">
+									<table style="font-family: Fira Sans,Helvetica,Arial,sans-serif; font-size: 16px; width:100%;  max-width:600px; background-color:#FFFFFF; border-radius:15px; margin-bottom:25px; color:#252627;">
 										<tbody>
 											<tr>
 												<td style="width: 100%;">
 													<table>
 														<tr>
 															<td style="padding:20px 20px 0 20px;" colspan="2">
-																<h3 style="font-size:1.25em; font-weight:600; margin-top: 0; margin-bottom:17px;"><?php echo $financ_node['title']; ?></h3>
-																<p style="font-size: .874em; line-height: 1.28em; margin:15px 0 0 0;font-style:italic; margin:0;"><?php echo $financ_node['teaser']; ?></p>				
+																<h3 style="font-family:Fira Sans,Helvetica,Arial,sans-serif!important; font-size:1.25em; font-weight:600; margin-top: 0; margin-bottom:17px;"><?php echo $financ_node['title']; ?></h3>
+																<p style="font-family:Fira Sans,Helvetica,Arial,sans-serif!important; font-size: .874em; line-height: 1.28em; margin:15px 0 0 0;font-style:italic; margin:0;"><?php echo $financ_node['teaser']; ?></p>				
 															</td>
 														</tr>
 														<tr>
-															<td style="padding:5px 10px 20px 20px;">
-																<p style="font-size: .95em; line-height: 1.35em; font-weight:800; margin:15px 0 0 0;">Convocant: <?php echo $financ_node['convocant']; ?></p>
-																<p style="font-size: .95em; line-height: 1.35em; font-weight:800; margin:0 0 0 0;">Termini: <?php echo $financ_node['termini']; ?></p>
+															<td style="padding:5px 10px 20px 20px; max-width:65%;">
+																<p style="font-family:Fira Sans,Helvetica,Arial,sans-serif!important;font-size: .95em; line-height: 1.35em; font-weight:800; margin:15px 0 0 0;">Convocant: <?php echo $financ_node['convocant']; ?></p>
+																<p style="font-family:Fira Sans,Helvetica,Arial,sans-serif!important;font-size: .95em; line-height: 1.35em; font-weight:800; margin:0 0 0 0;">Termini: <?php echo $financ_node['termini']; ?></p>
 															</td>
-															<td style="padding:5px 20px 20px 5px; vertical-align:bottom;">
-																<a href="<?php echo $financ_node['link']; ?>" style="background-color:#BE1622; color:#ffffff; font-size:14px; padding:14px 16px;float: right; border-radius: 5px; text-decoration:none; white-space:nowrap;">
+															<td style="padding:5px 20px 20px 5px; vertical-align:bottom;text-align:right;">
+																<a href="<?php echo $financ_node['link']; ?>" style="font-family:Fira Sans,Helvetica,Arial,sans-serif!important;background-color:#BE1622; color:#ffffff; font-size:14px; border:15px solid #BE1622;float: right; border-radius: 5px; text-decoration:none; white-space:nowrap;">
 																	Llegiu-ne més
 																</a>
 															</td>
@@ -307,8 +328,15 @@ ksort($financ_nodes);
 					<table style="background-color:#BE1622; border-radius:14px; width:100%;">
 						<tr>
 							<td style="vertical-align: bottom;">
-								<a href="https://xarxanet.org/financament" style="display:block; font-weight:800; color: #ffffff; font-size:1.68em; padding:70px 0 14px 22px; text-decoration:none;">
+								<a href="https://xarxanet.org/financament" style="font-family:Fira Sans,Helvetica,Arial,sans-serif!important;display:block; font-weight:800; color: #ffffff; font-size:1.68em; padding:70px 0 14px 22px; text-decoration:none;">
+								<!--[if (gte mso 9)|(IE)]>
+                                    <span>
+                                    <![endif]-->
 									Cerca el teu finançament
+									<!--[if (gte mso 9)|(IE)]>
+									</span>
+                                    <![endif]-->	
+							
 								</a>
 							</td>
 							<td style="color:#ffffff; padding:70px 22px 14px 0; text-align:right; vertical-align:bottom;">
@@ -323,8 +351,14 @@ ksort($financ_nodes);
 					<table style="background-color:#252627; border-radius:14px; padding-right:7px; width:100%;">
 						<tr>
 							<td>
-								<a href="https://xarxanet.org/financaments/premis" style="display:block; font-weight:800; color: #ffffff; font-size:1.68em; padding:70px 0 14px 22px; text-decoration:none;">
+								<a href="https://xarxanet.org/financaments/premis" style="font-family:Fira Sans,Helvetica,Arial,sans-serif!important;display:block; font-weight:800; color: #ffffff; font-size:1.68em; padding:70px 0 14px 22px; text-decoration:none;">
+									<!--[if (gte mso 9)|(IE)]>
+                                    <span style="border-bottom: 14px solid #252627; border-top:70px solid #252627">
+                                    <![endif]-->
 									Més premis
+									<!--[if (gte mso 9)|(IE)]>
+									</span>
+                                    <![endif]-->
 								</a>
 							</td>
 						</tr>
@@ -335,8 +369,14 @@ ksort($financ_nodes);
 					<table style="background-color:#252627; border-radius:14px; padding-left:7px; width:100%;">
 						<tr>
 							<td>
-								<a href="https://xarxanet.org/financaments/subvencions" style="display:block; font-weight:800; color: #ffffff; font-size:1.68em; padding:70px 0 14px 22px; text-decoration:none;">
+								<a href="https://xarxanet.org/financaments/subvencions" style="font-family:Fira Sans,Helvetica,Arial,sans-serif!important;display:block; font-weight:800; color: #ffffff; font-size:1.68em; padding:70px 0 14px 22px; text-decoration:none;">
+									<!--[if (gte mso 9)|(IE)]>
+                                    <span style="border-bottom: 14px solid #252627; border-top:70px solid #252627">
+                                    <![endif]-->
 									Més subvencions
+									<!--[if (gte mso 9)|(IE)]>
+									</span>
+                                    <![endif]-->
 								</a>
 							</td>
 						</tr>
@@ -347,12 +387,12 @@ ksort($financ_nodes);
 			<tr>
 				<td style="padding:0 7px 30px 15px">
 					<a href="https://twitter.com/ajuts_entitats">
-						<img width="270px" src="<?php echo $pathroot; ?>/sites/default/files/banner-twitter-financ.png" alt="Segueix al dia a Twitter @ajuts_entitats">
+						<img width="270" src="<?php echo $pathroot; ?>/sites/default/files/banner-twitter-financ.png" alt="Segueix al dia a Twitter @ajuts_entitats">
 					</a>
 				</td>
 				<td style="padding:0 7px 30px 15px">
 					<a href="https://xarxanet.org/projectes/noticies/calendari-de-convocatories-de-financament-anuals">
-						<img width="270px" src="<?php echo $pathroot; ?>/sites/default/files/banner-cal-financ_2.png" alt="Calendari 2021 - Calendari de convocatòries de finançament anuals 2021">
+						<img width="270" src="<?php echo $pathroot; ?>/sites/default/files/banner-cal-financ_2.png" alt="Calendari 2021 - Calendari de convocatòries de finançament anuals 2021">
 					</a>
 				</td>
 			</tr>
@@ -435,4 +475,9 @@ ksort($financ_nodes);
 			</tr>
 		</table>
 	</div>
+		<!--[if (gte mso 9)|(IE)]>
+		</td>
+		</tr>
+		</table>
+		<![endif]-->
 </center>
